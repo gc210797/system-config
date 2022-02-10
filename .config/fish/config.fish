e78a7675cfc57b57b21d759f4d17317a5c8763cb
gautam <gautam@192.168.1.8>
2022-02-11 00:11:26 +0530

    Fixed issue with nvim where fzf output showed gibberish in fist entry

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,10 @@
-theme_gruvbox dark hard
 alias ls=exa
 alias grep=rg
 alias vi=nvim
 alias vim=nvim
 
 if status --is-interactive
+  theme_gruvbox dark hard
   if ! set -q TMUX
   	exec tmux
   end
