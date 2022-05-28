03bba24dc74dab6ad249597e9adea58080dc09f7
Gautam <chandnagautam1@gmail.com>
2022-05-28 18:07:35 +0530

    Fix for navigation and selections for completions

--- .config/nvim/lua/cmp_setup.lua
+++ .config/nvim/lua/cmp_setup.lua
@@ -21,7 +21,7 @@ function M.setup()
 				luasnip.lsp_expand(args.body)
 			end
 		},
-		mapping = {
+		mapping = cmp.mapping.preset.insert({
 			['<Tab>'] = cmp.mapping(function(fallback)
 				if vim.fn.pumvisible() == 1 then
 					feedkey("<C-n>")
@@ -45,14 +45,15 @@ function M.setup()
 			['<C-d>'] = cmp.mapping.scroll_docs(-4),
 			['<C-f>'] = cmp.mapping.scroll_docs(4),
 			['<CR>'] = cmp.mapping.confirm({select = true})
-		},
-		sources = {
+		}),
+		sources = cmp.config.sources({
 			{name = 'nvim_lsp'},
 			{name = 'luasnip'},
 			{name = 'buffer'},
 			{name = 'crates'},
-			{name = 'orgmode'}
-		}
+			{name = 'orgmode'},
+            {name = 'treesitter'}
+		})
 	})
 end
 
