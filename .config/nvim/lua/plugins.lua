81163229b4631478450f2dd124944174495a505a
Gautam <chandnagautam1@gmail.com>
2022-07-17 20:07:17 +0530

    Markdown fix

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -49,9 +49,5 @@ return require('packer').startup(function()
 	use { 'TimUntersberger/neogit' }
 	use { 'sindrets/diffview.nvim' }
 	use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-    use({ "iamcco/markdown-preview.nvim",
-        run = "cd app && npm install",
-        setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
-        ft = { "markdown" },
-    })
+    use {'iamcco/markdown-preview.nvim', config = "vim.call('mkdp#util#install')"}
 end)
